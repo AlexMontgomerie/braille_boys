e6879b5bd1e927b03a257aeec2238a822064bb45
Henry Eshbaugh <he915@ic.ac.uk>
2017-03-04 16:43:28 +0000

    Char-to-braille mapping Bytes are structured like this: x x 5 4 3 2 1 0 5 corresponds to the state of motor 5 (1=running, 0=stopped), and so on

---
+++ braille-xlate/genmap.py
@@ -0,0 +1,18 @@
+m = open('map.dat', 'r')
+lines = m.readlines()
+m.close()
+tups = []
+for i in range(len(lines)):
+    tups.append((lines[i], i))
+tups = sorted(tups, key = lambda x: x[0])
+hdr = open("braillemap.h", "w")
+hdr.write("#ifndef _braillemap_h\n")
+hdr.write("#define _braillemap_h\n")
+hdr.write("#include <stdint.h>\n")
+hdr.write("const uint8_t _bmap[] = { ")
+map(lambda x: hdr.write(str(x[1]) + ","), tups)
+hdr.write(" };\n")
+hdr.write("static inline uint8_t braille_lookup(char ch)\n")
+hdr.write("{ return _bmap[ch]; }\n")
+hdr.write("#endif // !defined(_braillemap_h")
+hdr.close()
